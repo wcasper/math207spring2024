e67760b91093c77d26221bdd7687d1eb8e7bd9cd
wcasper <williamrileycasper@gmail.com>
2024-01-22 16:58:04 -0800

    updated

--- _topics/matlab/topic_004_autonomous.m
+++ _topics/matlab/topic_004_autonomous.m
@@ -12,7 +12,7 @@ ymax =  2.0;    % maximum y value
 % plot the logistic equation phase diagram
 k = 2;   % growth rate
 C = 1;   % carrying capacity
-t     = linspace(xmin,xmax,20);
+t     = linspace(xmin,xmax,nx);
 inits = [0.1,0.5,1.1,1.5,2.0];  % starting points
 
 hold on
@@ -36,6 +36,43 @@ saveas(gcf,'../fig/004-logistic.png')
 
 
 
+% plot some integral curves of the latter slope field
+
+equilib = [-2,-1,0,1]
+
+clf 
+hold on
+
+ystarts = [-2.1,-1.5,-1.1,-0.9,-0.5,0.1,0.5,0.9,1.01]
+
+ncurves = length(ystarts)
+
+fun1 = @(x,y)((y-1)*y*(y+1)*(y+2)*(y+2))
+
+for m = 1:ncurves
+  Z = zeros([1,nx]);
+  Z(1) = ystarts(m);
+  x = xmin;
+  dx = (xmax-xmin)/nx;
+  nsteps = 1000;  % steps per grid point
+  for k = 2:nx
+    Z(k) = Z(k-1);
+    foo = Z(k-1);
+    for j = 1:nsteps
+      x = x + dx/nsteps;
+      foo = foo + (dx/nsteps)*fun1(x,foo);
+    end
+    Z(k) = foo;
+  end
+  plot(linspace(xmin,xmax,nx),Z,'Linewidth',2)
+end
+
+for e = equilib
+  yline(e,'r-.','LineWidth',4)
+end
+
+ylim([-2.5,1.2])
   
+saveas(gcf,'../fig/004-phase.png')
 
 
